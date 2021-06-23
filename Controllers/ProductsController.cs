1a9e567763d2f83d6b6748aa7e9b1ae3c4ae87c0
Kennedy Ngure <kngure@fintech-group.com>
2021-06-23 10:55:57 +0300

    test product api and remove the api from weather forecast

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace CRUD_WEB_API_SP_MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string products()
+        {
+            return "Test Product";
+        }
+    }
+}
